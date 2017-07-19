e7de4339ebccacc5411f3feee4311f5a647ed72b
Kouji Matsui <k@kekyo.net>
2017-07-19 17:39:24 +0900

    Turn literal to symbolic.

--- Nesp.Core.Tests/NespTests.cs
+++ Nesp.Core.Tests/NespTests.cs
@@ -119,35 +119,35 @@ namespace Nesp.Core.Tests
         public void SpaceOneTest()
         {
             var actual = Parse("123456 abc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace  ) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
 
         [Test]
         public void SpaceTwoTest()
         {
             var actual = Parse("123456  abc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace   ) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
 
         [Test]
         public void TabOneTest()
         {
             var actual = Parse("123456\tabc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace \\t) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
 
         [Test]
         public void ReturnOneTest()
         {
             var actual = Parse("123456\rabc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace \\r) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
 
         [Test]
         public void NewlineOneTest()
         {
             var actual = Parse("123456\nabc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace \\n) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
         #endregion
 
@@ -163,14 +163,14 @@ namespace Nesp.Core.Tests
         public void ListTwoTest()
         {
             var actual = Parse("123456 abc.def", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace  ) (token (id abc.def)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)))", actual);
         }
 
         [Test]
         public void ListThreeTest()
         {
             var actual = Parse("123456 abc.def -123.456", p => p.list());
-            Assert.AreEqual("(list (token (numeric 123456)) (whiteSpace  ) (token (id abc.def)) (whiteSpace  ) (token (numeric -123.456)))", actual);
+            Assert.AreEqual("(list (token (numeric 123456)) (token (id abc.def)) (token (numeric -123.456)))", actual);
         }
         #endregion
 
@@ -179,21 +179,21 @@ namespace Nesp.Core.Tests
         public void ExpressionOneValueTest()
         {
             var actual = Parse("(123456)", p => p.expression());
-            Assert.AreEqual("(expression (bracketLeft () (list (token (numeric 123456))) (bracketRight )))", actual);
+            Assert.AreEqual("(expression ( (list (token (numeric 123456))) ))", actual);
         }
 
         [Test]
         public void ExpressionTwoValuesTest()
         {
             var actual = Parse("(123456 abc.def)", p => p.expression());
-            Assert.AreEqual("(expression (bracketLeft () (list (token (numeric 123456)) (whiteSpace  ) (token (id abc.def))) (bracketRight )))", actual);
+            Assert.AreEqual("(expression ( (list (token (numeric 123456)) (token (id abc.def))) ))", actual);
         }
 
         [Test]
         public void ExpressionNestedValuesTest()
         {
             var actual = Parse("(123456 (abc.def -123.456))", p => p.expression());
-            Assert.AreEqual("(expression (bracketLeft () (list (token (numeric 123456)) (whiteSpace  ) (token (expression (bracketLeft () (list (token (id abc.def)) (whiteSpace  ) (token (numeric -123.456))) (bracketRight ))))) (bracketRight )))", actual);
+            Assert.AreEqual("(expression ( (list (token (numeric 123456)) (token (expression ( (list (token (id abc.def)) (token (numeric -123.456))) )))) ))", actual);
         }
         #endregion
     }
--- Nesp.Core/Nesp.g4
+++ Nesp.Core/Nesp.g4
@@ -1,22 +1,28 @@
 ﻿grammar Nesp;
 
-expression : bracketLeft whiteSpace? list whiteSpace? bracketRight ;
-list : token (whiteSpace? token)* ;
+expression : BRACKETLEFT WHITESPACE? list WHITESPACE? BRACKETRIGHT ;
+list : token (WHITESPACE? token)* ;
 token : numeric | id | expression ;
 numeric : NUMERIC ;
-id : COMPLEXSYMBOL ;
-
-bracketLeft : BRACKETLEFT ;
-bracketRight : BRACKETRIGHT ;
-whiteSpace : WHITESPACE ;
+id : ID ;
 
 /////////////////////////////////////////////////////////////////////
 
-NUMERIC : ('+'|'-')? ('0'..'9')+ ('.' ('0'..'9')*)? ;
-COMPLEXSYMBOL : SYMBOL ('.' SYMBOL)* ;
-SYMBOL : ('a'..'z'|'A'..'Z'|'_') ('a'..'z'|'A'..'Z'|'0'..'9'|'_')* ;
+ID : COMPLEXSYMBOL (WHITESPACE GREATER WHITESPACE ID WHITESPACE (COMMA WHITESPACE ID)* LESS)* ;
+COMPLEXSYMBOL : SYMBOL (PERIOD SYMBOL)* ;
+SYMBOL : [a-zA-Z_] [a-zA-Z0-9_]* ;
+NUMERIC : (PLUS|MINUS)? [0-9]+ (PERIOD [0-9]*)? ;
 
+//DOUBLEQUOTE : '"' -> channel(HIDDEN) ;
 BRACKETLEFT : '(' ;
 BRACKETRIGHT : ')' ;
+GREATER : '<' ;
+LESS : '>' ;
 
-WHITESPACE : (' '|'\t'|'\r'|'\n')+ ;
+ESCAPE : '\\' ;
+COMMA : ',' ;
+PERIOD : '.' ;
+PLUS : '+' ;
+MINUS : '-' ;
+
+WHITESPACE : [ \t\r\n]+ -> channel(HIDDEN) ;
